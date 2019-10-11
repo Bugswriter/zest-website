d6b63aa33772f2f615eb1cbda55b64c7cff2abbb
Suraj <bugswriter@protonmail.com>
2019-10-11 12:22:08 +0000

    server commit

--- zestpkg/static/js/script.js
+++ zestpkg/static/js/script.js
@@ -3,7 +3,7 @@ $image_crop = $('#cropper').croppie({
 	viewport: {
 	  width:300,
 	  height:300,
-	  type:'square' 
+	  type:'circle' 
 	},
 	boundary:{
 	  width:300,
@@ -13,38 +13,50 @@ $image_crop = $('#cropper').croppie({
 
 $(document).ready(function() {
   
-  setTimeout(function(){
+    setTimeout(function(){
   	$('#flashmessage').hide('slow');
-  }, 4000);
+    }, 4000);
 
+    $image_crop = $('#cropper').croppie({
+	enableExif: true,
+	viewport: {
+	  width:300,
+	  height:300,
+	  type:'circle' 
+	},
+	boundary:{
+	  width:300,
+	  height:300
+	}
+    });
 
-	$('#uploadimage').on('change', function(){
-		var reader = new FileReader();
-		reader.onload = function (event) {
-		$image_crop.croppie('bind', {
+    $('#uploadimage').on('change', function(){
+	var reader = new FileReader();
+	reader.onload = function (event) {
+	    $image_crop.croppie('bind', {
 	    	url: event.target.result
 	    }).then(function(){
 	    	console.log('jQuery bind complete');
 	    });
 	}
-		reader.readAsDataURL(this.files[0]);
-		$('#cropimage').modal('show');
-	});
-
-	$('#uploadbtn').click(function(event){
-		$image_crop.croppie('result', {
-		type: 'canvas',
-		size: 'viewport'
-		}).then(function(response){
-			$.ajax({
-				url:"/upload_profile",
-				type: "POST",
-				data:{"image": response},
-				success:function(data){
-					$('#cropimage').modal('hide');
-					$('#feedback').html("Profile picture updated"); 
-				}
-			});
-		})
-	});
+	reader.readAsDataURL(this.files[0]);
+	$('#cropimage').modal('show');
+    });
+    
+    $('#uploadbtn').click(function(event){
+	$image_crop.croppie('result', {
+	    type: 'canvas',
+	    size: 'viewport'
+	}).then(function(response){
+	    $.ajax({
+		url:"/upload_profile",
+		type: "POST",
+		data:{"image": response},
+		success:function(data){
+		    $('#cropimage').modal('hide');
+		    $('#feedback').html("Profile picture updated"); 
+		}
+	    });
+	})
+    });
 });
