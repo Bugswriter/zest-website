5a2be330c7b530b8ef335fc432adea7bcbac38d7
bugswriter <surajkushwah6969@gmail.com>
2019-11-01 02:35:59 +0530

    email shit

--- zestpkg/main/routes.py
+++ zestpkg/main/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 main = Blueprint('main', __name__)
 
@@ -12,6 +12,23 @@ def home():
 @main.route('/stats', methods=['GET', 'POST'])
 def stats():
 	return render_template('stats.html', title='Statistics')
+
+
+
+@main.route('/stat_data', methods=['GET', 'POST'])
+def stat_data():
+	data = {
+		'chart1': [34,56,76,23,12],
+		'chart2': [36,67,23,51,65,44],
+		'chart3': [5,78,457,31,46,7]
+	}
+	return jsonify(data)
+
+
+@main.route('/emailtest')
+def emailtest():
+	data = {'username': 'Bugswriter', 'token': '#'}
+	return render_template('account_email.html', **data)
 
 
 '''
--- zestpkg/users/utils.py
+++ zestpkg/users/utils.py
@@ -1,29 +1,24 @@
 from zestpkg import mail
 from flask_mail import Message
-from flask import url_for, current_app
+from flask import url_for, current_app, render_template
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 def send_confirmation_link(username, email, password):
 	s=Serializer(current_app.config['SECRET_KEY'], 1800)
 	token= s.dumps({'username': username,'email': email, 'password':password}).decode('utf-8')
-	msg=Message('Verify your Account for Zest registeration account',
-				sender='akashraj5399@gmail.com',
+	msg = Message('Confirm your email',
+				sender='admin@zest2019.in',
 				recipients=[email])
-	msg.body='''Zest2019 - To Confirm your Account, visit following link:
-				{}
-				If you did not make this request then simply ignore this email.
-			'''.format(url_for('users.confirmation', token=token, _external=True))
+	data = {'username': username, 'token': token}
+	msg.html = render_template('account_email.html', **data)
 	mail.send(msg)
 
 
 def send_reset_email(user):
 	token=user.get_reset_token()
-	msg=Message('Password Reset Request',
-				sender='akashraj5399@gmail.com',
+	msg = Message('Password Reset Request',
+				sender='admin@zest2019.in',
 				recipients=[user.email])
-	msg.body='''
-			To Reset your password, visit following link:
-				{}
-				If you did not make this request then simply ignore this email.
-			'''.format(url_for('users.reset_token', token=token, _external=True))
+	data = {'username': user.username, 'token': token}
+	msg.html = render_template('reset_pass_email.html', **data)
 	mail.send(msg)
