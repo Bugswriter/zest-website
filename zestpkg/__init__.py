228bb325e89d9657cae294d8c9d8a959e1cb9cb4
Suraj Kushwah <surajkushwah6969@gmail.com>
2019-11-04 00:15:41 +0530

    email changed to zest2k19

--- zestpkg/__init__.py
+++ zestpkg/__init__.py
@@ -13,9 +13,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'akashraj5399@gmail.com'
-#app.config['MAIL_PASSWORD'] = 'uxsaaqoabuaplxxz'
-app.config['MAIL_PASSWORD'] = 'vgdvirbrjdlakbky'
+app.config['MAIL_USERNAME'] = 'zest2k19@gmail.com'
+#app.config['MAIL_PASSWORD'] = 'uxsaaqoabuaplxxz' admin@zest2019.in
+#app.config['MAIL_PASSWORD'] = 'vgdvirbrjdlakbky' akashraj5399
+app.config['MAIL_PASSWORD'] = 'ypuhhcnuxhawaswq'
 
 
 
